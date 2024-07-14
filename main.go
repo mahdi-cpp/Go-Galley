fcb116052aafa98dd560195f5cccddfe88cb724b
Mahdi <mahdi.cpp@gmail.com>
2024-07-14 20:24:37 +0330

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package Go_Galley
+
+import (
+	"Go-Galley/models"
+	"github.com/mahdi.cpp/Go-Gallery/models"
+)
+
+func main() {
+
+	models.
+}
---
+++ models/model.go
@@ -0,0 +1,6 @@
+package models
+
+type music struct {
+	Name   string `json:"name"`
+	Artist string `json:"artist"`
+}
